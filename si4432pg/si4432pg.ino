f204780b9a4a9d96a238460cb71204580f1fc5ed
pylover <vahid.mardani@gmail.com>
2022-01-25 02:32:53 +0330

    Migrate to nash >= 2.0.0

--- si4432pg/si4432pg.ino
+++ si4432pg/si4432pg.ino
@@ -1,9 +1,12 @@
-#include <nash.h>
 #include <RH_RF22.h>
+#include <nash.h>
 
+#define BUSY_LED_PIN 9 
+#define SDN_PIN	7 
+#define STANDBY_LED_PIN	8 
 
+Nash shell("si4432-# ", BUSY_LED_PIN);
 RH_RF22 rf22;
-#define SDN	7 
 
 
 void printbits(uint8_t value) {
@@ -41,31 +44,21 @@ int parseint(const char *str) {
 }
 
 
-int register_set(size_t argc, char **argv, struct process *self) {
-	if (argc != 3) {
-		nash_print_usage(self->executable, true);
-		return EXIT_FAILURE;
-	}
-	
-	unsigned char addr = parseint(argv[1]);
-	unsigned char value = parseint(argv[2]);
+int8_t register_set(Nash::Process *self) {
+	unsigned char addr = parseint(self->argv[1]);
+	unsigned char value = parseint(self->argv[2]);
 	rf22.spiWrite(addr, value);
 	return EXIT_SUCCESS;
 }
 
 
-int register_get(size_t argc, char **argv, struct process *self) {
-	if (argc < 2 || argc > 3) {
-		nash_print_usage(self->executable, true);
-		return EXIT_FAILURE;
-	}
-
-	unsigned char addr = parseint(argv[1]);
+int8_t register_get(Nash::Process *self) {
+	unsigned char addr = parseint(self->argv[1]);
 	uint8_t value = rf22.spiRead(addr);
 	
-	if (argc == 3) {
+	if (self->argc == 3) {
 		char * next;
-		uint8_t msb = strtol(argv[2], &next, 10);
+		uint8_t msb = strtol(self->argv[2], &next, 10);
 		uint8_t lsb = strtol(++next, NULL, 10);
 		value <<= 7 - msb;
 		value >>= (7 - msb) + lsb;
@@ -80,19 +73,16 @@ int register_get(size_t argc, char **argv, struct process *self) {
 }
 
 
-int power(size_t argc, char **argv, struct process *self) {
-	if (argc > 2) {
-		nash_print_usage(self->executable, true);
-		return EXIT_FAILURE;
-	}
-	
-	if (argc == 2) {
+int8_t power(Nash::Process *self) {
+	if (self->argc == 2) {
 		/* Modify the gpio */
-		digitalWrite(SDN, argv[1][1] == 'f');
+		bool off = self->argv[1][1] == 'f';
+		digitalWrite(SDN_PIN, off);
+		digitalWrite(STANDBY_LED_PIN, off);
 	}
 
 	/* Just report the power status */
-	bool status = digitalRead(SDN);
+	bool status = digitalRead(SDN_PIN);
 	PRINT("Power is ");
 	PRINTLN(status? "Off": "On");
 	return EXIT_SUCCESS;
@@ -100,21 +90,43 @@ int power(size_t argc, char **argv, struct process *self) {
 }
 
 
+int8_t sleep(Nash::Process *self) {
+	if (self->signal == SIG_INT) {
+		return EXIT_FAILURE;
+	}
+	unsigned long towait = atol(self->argv[1]) * 1000;
+	unsigned long taken = millis() - self->starttime;
+	if (taken < towait) {
+		return ALIVE;
+	}
+	return EXIT_SUCCESS;
+}
 
-static struct executable programs[] = {
-	{"help", NULL, nash_help},
-	{"free", NULL, nash_free},
-	{"reg-set", "ADDR VALUE", register_set},
-	{"reg-get", "ADDR [MSB:LSB]", register_get},
-	{"power", "[ON/OFF]", power},
+
+static Nash::Executable programs[] = {
+	{"free", 0, 0, NULL, printFreeMemory},
+	{"reg-set", 2, 2, "ADDR VALUE", register_set},
+	{"reg-get", 1, 2, "ADDR [MSB:LSB]", register_get},
+	{"power", 0, 1, "[ON/OFF]", power},
+	{"sleep", 1, 1, "NUMBER", sleep},
 	{ NULL }
 };
 
 
 void setup() {
-	nash_init(programs);
-	pinMode(SDN, OUTPUT);
-	digitalWrite(SDN, LOW);
+	/* GPIO setup */
+	pinMode(SDN_PIN, OUTPUT);
+	pinMode(STANDBY_LED_PIN, OUTPUT);
+	digitalWrite(SDN_PIN, LOW);
+	digitalWrite(STANDBY_LED_PIN, LOW);
+	
+	/* UART */
+	Serial.begin(115200);
+	
+	/* Shell */
+	shell.init(programs);
+	
+	/* RF22 */
 	// Defaults after init are 434.0MHz, 0.05MHz AFC pull-in, 
 	// modulation FSK_Rb2_4Fd36
 	if (!rf22.init()) {
@@ -123,5 +135,5 @@ void setup() {
 }
 
 void loop() {
-	nash_loop();
+	shell.loop();
 }
